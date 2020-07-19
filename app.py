16de1c7cad71bebe47ae4a15aa7f305502bd41bd
WendaDeng <dwd1044898101@gmail.com>
2020-07-19 16:31:58 +0800

    add video localize funcitonality

--- app.py
+++ app.py
@@ -2,20 +2,20 @@ from __future__ import print_function
 
 from collections import defaultdict
 
-from flask import Flask, render_template, make_response
-from flask import redirect, request, jsonify, url_for
+from flask import Flask, render_template, make_response, request, jsonify
+from flask import flash, redirect, url_for
+from werkzeug.utils import secure_filename
 
-import io
 import os
-import uuid
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
-import numpy as np
+import datetime
+import subprocess
 
 app = Flask(__name__)
 app.secret_key = 's3cr3t'
 app.debug = True
 app._static_folder = os.path.abspath("templates/static/")
+app._upload_folder = os.path.abspath("templates/upload")
+ALLOWED_EXTENSIONS = {'mp4', 'avi', 'png', 'jpg', 'jpeg', 'gif'}
 
 result_num = 20
 
@@ -53,17 +53,17 @@ for line in lines:
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('layouts/index.html', title='Multimedia Search Engine')
+    return render_template('layouts/index.html')
 
 
 @app.route('/video_search', methods=['GET'])
 def video_search():
-    return render_template('layouts/video_search.html', title='Video Search')
+    return render_template('layouts/video_search.html')
 
 
 @app.route('/video_localize', methods=['GET'])
 def video_localize():
-    return render_template('layouts/video_localize.html', title='Video Localize')
+    return render_template('layouts/video_localize.html')
 
 
 @app.route('/search', methods=['POST'])
@@ -84,77 +84,76 @@ def search():
     return jsonify(params)
 
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        print(file.filename)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            print(filename)
+            # 若不存在此目录，则创建之
+            if not os.path.isdir(app._upload_folder):
+                os.makedirs(app._upload_folder)
+
+            file.save(os.path.join(app._upload_folder, filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+
+@app.route('/uploaded', methods=['GET', 'POST'])
+def uploaded_file():
+	return '''
+	<!doctype html>
+	<title>Uploaded the file</title>
+	<h1> File has been Successfully Uploaded </h1>
+	'''
+
+
 @app.route('/localize', methods=['POST'])
 def localize():
-    print('lllll')
-    localize_str = request.form['localize_str'][1:-1]
-    upload_video = request.form['upload_video']
+    localize_str = request.form['localize_str']
+    filename = request.form['filename']
+    print(localize_str)
+    print(filename)
     query_id = tacos_query_dict[localize_str.lower()] if localize_str.lower() in tacos_query_dict else None
     query_result = tacos_result_dict[upload_video + '_' + query_id] if query_id else None
-
+    
     rank, scores, video_names = [], [], []
-    for result in query_result:
-        # result: score_rank_start-time_end-time
-        data = result.split(',')
-        rank.append(int(data[1]))
-        # origin-video-name_score_start-time_end-time_query-id_rank
-        video_names.append(upload_video + '_' + data[0] + '_' + data[2] + '_' + data[3] +
-                           '_' + query_id + '_' + data[1])
-        scores.append(data[0])
-    # rank.sort()
-
+    # for result in query_result:
+    #     # result: score_rank_start-time_end-time
+    #     data = result.split(',')
+    #     rank.append(int(data[1]))
+    #     # origin-video-name_score_start-time_end-time_query-id_rank
+    #     video_names.append(upload_video + '_' + data[0] + '_' + data[2] + '_' + data[3] +
+    #                        '_' + query_id + '_' + data[1])
+    #     scores.append(data[0])
+    
     params = {'video_names': video_names, 'scores': scores, 'idxs': rank}
     return jsonify(params)
-
-
-# 原有项目方法，展示结果
-@app.route('/results/<uuid>', methods=['GET'])
-def results(uuid):
-    title = 'Result'
-    data = get_file_content(uuid)
-    return render_template('layouts/results.html',
-                           title=title,
-                           data=data)
-
-
-# 原有项目方法，获取canvas数据
-@app.route('/postmethod', methods=['POST'])
-def post_javascript_data():
-    js_data = request.form['canvas_data']
-    unique_id = create_csv(js_data)
-    params = {'uuid': unique_id}
-    return jsonify(params)
-
-
-# 原有项目方法，将结果画到canvas上
-@app.route('/plot/<imgdata>')
-def plot(imgdata):
-    data = [float(i) for i in imgdata.strip('[]').split(',')]
-    data = np.reshape(data, (200, 200))
-    fig = Figure()
-    axis = fig.add_subplot(1, 1, 1)
-    axis.axis('off')
-    axis.imshow(data, interpolation='nearest')
-    canvas = FigureCanvas(fig)
-    output = io.BytesIO()
-    canvas.print_png(output)
-    response = make_response(output.getvalue())
-    response.mimetype = 'image/png'
-    return response
-
-
-# 原有画图项目方法，给前端传递过来的text生成id
-def create_csv(text):
-    unique_id = str(uuid.uuid4())
-    with open('images/' + unique_id + '.csv', 'a') as file:
-        file.write(text[1:-1] + "\n")
-    return unique_id
-
-
-# 原有画图项目方法，根据id获取保存的text内容
-def get_file_content(uuid):
-    with open('images/' + uuid + '.csv', 'r') as file:
-        return file.read()
 
 
 if __name__ == '__main__':
--- templates/static/js/script.js
+++ templates/static/js/script.js
@@ -1,83 +1,7 @@
 $( document ).ready(function() {
-  // 原有画图项目的方法，创建新的画图框
-   function createCanvas(parent, width, height) {
-    var canvas = document.getElementById("inputCanvas");
-    canvas.context = canvas.getContext('2d');
-    return canvas;
-  }
-
-  // 原有画图项目的方法
-  function init(container, width, height, fillColor) {
-    var canvas = createCanvas(container, width, height);
-    var ctx = canvas.context;
-    ctx.fillCircle = function(x, y, radius, fillColor) {
-      this.fillStyle = fillColor;
-      this.beginPath();
-      this.moveTo(x, y);
-      this.arc(x, y, radius, 0, Math.PI * 2, false);
-      this.fill();
-    };
-    ctx.clearTo = function(fillColor) {
-      ctx.fillStyle = fillColor;
-      ctx.fillRect(0, 0, width, height);
-    };
-    ctx.clearTo("#fff");
-
-    canvas.onmousemove = function(e) {
-      if (!canvas.isDrawing) {
-        return;
-      }
-      var x = e.pageX - this.offsetLeft;
-      var y = e.pageY - this.offsetTop;
-      var radius = 10;
-      var fillColor = 'rgb(102,153,255)';
-      ctx.fillCircle(x, y, radius, fillColor);
-    };
-    canvas.onmousedown = function(e) {
-      canvas.isDrawing = true;
-    };
-    canvas.onmouseup = function(e) {
-      canvas.isDrawing = false;
-    };
-  }
-
-  // 原有画图项目的方法，清理画图框中的数据
-  function clearCanvas() {
-    var canvas = document.getElementById("inputCanvas");
-    var ctx = canvas.getContext("2d");
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-  }
-
-  // 原有画图项目的方法，获得画图框中的数据
-  function getData() {
-    console.log('hello');
-    var canvas = document.getElementById("inputCanvas");
-    var imageData = canvas.context.getImageData(0, 0, canvas.width, canvas.height);
-    var data = imageData.data;
-    var outputData = []
-    for(var i = 0; i < data.length; i += 4) {
-      var brightness = 0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2];
-      outputData.push(brightness);
-    }
-    $.post( "/postmethod", {
-      canvas_data: JSON.stringify(outputData)
-    }, function(err, req, resp){
-      window.location.href = "/results/"+resp["responseJSON"]["uuid"];  
-    });
-  }
-
-// 原有画图项目的方法，清空数据
-  $( "#clearButton" ).click(function(){
-    clearCanvas();
-  });
-
-  // 原有画图项目的方法，发送数据
-  $( "#sendButton" ).click(function(){
-    getData();
-  });
 
   // 遍历结果，然后逐条清除
-  function removeItem(var elem_id){
+  function removeItem(elem_id){
     var e = document.getElementById(elem_id);
     var first = e.firstElementChild;
     while (first) {
@@ -94,7 +18,6 @@ $( document ).ready(function() {
 
   // 发送搜索数据
   function sendSearchData() {
-    // var data = document.getElementById("search");
 	var data = $("#search").val();
 	console.log(data);
 	$.post( "/search", {
@@ -105,7 +28,6 @@ $( document ).ready(function() {
       var idxs = resp["responseJSON"]['idxs'];
       console.log(video_names)
       console.log(scores)
-      console.log(idxs)
       if (Array.isArray(video_names) && video_names.length) {
         for(var i in idxs) {
           var str = '<div> <video width="280" height="230" controls>'
@@ -125,8 +47,9 @@ $( document ).ready(function() {
   //实例化一个plupload上传对象
   var uploader = new plupload.Uploader({
       browse_button : 'browse', //触发文件选择对话框的按钮，为那个元素id
-      url : '/localize' //服务器端的上传页面地址
+      url : '/upload' //服务器端的上传页面地址
   });
+  var filename = '';
 
   //在实例对象上调用init()方法进行初始化
   uploader.init();
@@ -136,55 +59,67 @@ $( document ).ready(function() {
       //每个事件监听函数都会传入一些很有用的参数，
       //我们可以利用这些参数提供的信息来做比如更新UI，提示上传进度等操作
     plupload.each(files, function (file) {
-      uploader_video.start();
+      filename = file.name;
+      uploader.start();
     });
   });
 
   //上传中，显示进度条
   uploader.bind('UploadProgress', function(uploader, file) {
-    $('#Progress').css('width', file.percent+'%');
-  };
+    $('#progress').css('width', file.percent+'%');
+  });
 
+  // 上传完成
+  uploader.bind('UploadComplete', function(uploader, files) {
+    plupload.each(files, function (file) {
+      alert(file.name + ' uploaded successfully!');
+    })
+  });
+
+  // 上传出错
   uploader.bind('Error', function(uploader, errObject) { //上传出错的时候触发
-        alert(errObject.code+errObject.message);
+    alert(errObject.code+errObject.message);
   });
 
   //最后给"开始上传"按钮注册事件
-  document.getElementById('start_upload').onclick = function(){
+  $( "#start_upload" ).click(function(){
+    var localize_str = $("#localize").val();
+    console.log(localize_str);
+    if (localize_str == '') {
+      alert('Please input localize string first!')
+      return;
+    }
+    if (filename == '') {
+      alert('Please upload video file first!')
+      return;
+    }
     removeItem("localizationResult");
-    uploader.start(); //调用实例对象的start()方法开始上传文件，当然你也可以在其他地方调用该方法
-    alert(file.name);
-    sendLocalizationData(file.name);
-  };
-
-  // 发送定位数据
-  function sendLocalizationData(var filename) {
-	var localize_str = $("#localize").val();
-	console.log(localize_str, filename);
-	$.post( "/localize", {
-      localize_str: JSON.stringify(localize_str)
-      upload_video: JSON.stringify(filename)
-    }, function(err, req, resp){
-      var video_names = resp["responseJSON"]['video_names'];
-      var scores = resp["responseJSON"]['scores'];
-      var idxs = resp["responseJSON"]['idxs'];
-      console.log(video_names)
-      console.log(scores)
-      console.log(idxs)
-      if (Array.isArray(video_names) && video_names.length) {
-        for(var i in idxs) {
-          var str = '<div> <video width="280" height="230" controls>'
-            + '<source src="movie.mp4" type="video/mp4">'
-            + '</video>'
-            + '<p>message</p> </div>';
-          video_str = str.replace('movie', '/static/videos/' + video_names[i]);
-          video_str = video_str.replace('message', '<em>Top-' + i + '\t<em>Score:' + scores[i]);
-          $( "#localizationResult" ).append(video_str)
-        };
-      } else {
-        $( "#localizationResult" ).append('<p>Do not find any related videos. Please try other queries.</p>')
-      }
-    });
-  }
+    
+	  console.log(localize_str, filename);
+    $.post( "/localize", {
+        localize_str: JSON.stringify(localize_str),
+        filename: JSON.stringify(filename)
+      }, function(err, req, resp){
+        var video_names = resp["responseJSON"]['video_names'];
+        var scores = resp["responseJSON"]['scores'];
+        var idxs = resp["responseJSON"]['idxs'];
+        console.log(video_names)
+        console.log(scores)
+        console.log(idxs)
+        if (Array.isArray(video_names) && video_names.length) {
+          for(var i in idxs) {
+            var str = '<div> <video width="280" height="230" controls>'
+              + '<source src="movie.mp4" type="video/mp4">'
+              + '</video>'
+              + '<p>message</p> </div>';
+            video_str = str.replace('movie', '/static/videos/' + video_names[i]);
+            video_str = video_str.replace('message', '<em>Top-' + i + '\t<em>Score:' + scores[i]);
+            $( "#localizationResult" ).append(video_str)
+          };
+        } else {
+          $( "#localizationResult" ).append('<p>Do not find any related videos. Please try other queries.</p>')
+        }
+      });
+  });
 
 });
